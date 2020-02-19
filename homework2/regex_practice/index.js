78d1e94379586c96fa6707ac257fc5fb1d7a70ac
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2020-02-19 12:20:29 -0800

    added isVisa, isMasterCard
    Both pass basic tests
    Slight modifications to isEightThroughTwentyNine & isAdaFloat

--- homework2/regex_practice/index.js
+++ homework2/regex_practice/index.js
@@ -1,19 +1,30 @@
 function isCanadianPostalCode(s) {
     return /[A-Z]\d[A-Z] \d[A-Z]\d/.test(s);
 }
+
+function isVisa(s) {
+    return /^4(\d{15}|\d{12})$/.test(s);
+}
+
+function isMasterCard(s) {
+    return /^5[1-5]\d{14}$/.test(s);
+}
+
 function isNotThreeEndingInOO(s) {
-	return /(^$|.{3}(?<![oO]{2}))/.test(s);
+    return /(^$|.{3}(?<![oO]{2}))/.test(s);
 }
+
 function isEightThroughTwentyNine(s) {
-    return /[89]|[12]\d/.test(s);
+    return /^[89]|[12]\d$/.test(s);
 }
+
 function isAdaFloat(s) {
-    return  /\d(_?\d)*
-            (\.\d(_?\d))?
-            ((E+?(_?\d)*)|(E-(_?\d)))*/.test(s);
+    return /^\d(_?\d)*(\.\d(_?\d)*)?(([Ee]\+?\d(_?\d)*)|([Ee]\-\d(_?\d)*))?$/.test(s);
 }
 module.exports = {
     isCanadianPostalCode,
+    isVisa,
+    isMasterCard,
     isAdaFloat,
     isEightThroughTwentyNine,
     isNotThreeEndingInOO
