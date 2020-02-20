aa76cf2607c7118c806f66f49e890e7f50c254db
Jialing Li <Linannan0505@gmail.com>
2020-02-20 14:51:30 -0800

    Qiyue started ohm

---
+++ homework2/ohm_practice/index.js
@@ -0,0 +1,13 @@
+const ohm = require('ohm-js');
+
+function isCanadianPostalCode(s) {
+    const grammar = ohm.grammar(` CPC {
+        c = "A".."Z" digit "A".."Z" " " digit "A".."Z" digit
+    }`);
+    return grammar.match(s).succeeded();
+}
+
+module.exports = {
+    isCanadianPostalCode,
+    
+};
